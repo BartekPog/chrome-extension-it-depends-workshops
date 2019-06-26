11c4f0a2f89dac1ec1f61f9b203d05656760b68f
Bartłomiej Pogodziński <bartek.pogod@gmail.com>
2019-06-27 00:15:38 +0200

    Update content for better readability

--- content.js
+++ content.js
@@ -1,18 +1,13 @@
 //jshint esversion:8
 
 (async () => {
-    const src = chrome.extension.getURL('lib/wordsCounter.js');
-    const counter = await import(src);
+  const src = chrome.extension.getURL('lib/wordsCounter.js');
+  const counter = await import(src);
 
 
   let words = counter.count(document.body);
 
-
   let time = Math.round(words/25)/10;
-
-
-
-  //alert(time);
 
   chrome.runtime.sendMessage({
   message: 'show-badge',
@@ -21,18 +16,3 @@
 
 
  })();
-// $('*').filter(function()
-// {
-//     if(($(this).text().lenght>0)&&($(this).text().lenght<100))
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//         return false;
-//     }
-// }).each(function()
-// {
-//     alert($(this).text());
-// });
-// //   console.log(words);
--- popup/scripts.js
+++ popup/scripts.js
@@ -1,9 +1,11 @@
-(() => {
+//jshint esversion:8
 
+(() => {
   $(document).ready(() => {
     const wordsCountElement = $('#wordsCount').first();
     const minutesElement = $('#minutes').first();
   });
+  
 
 
 })();
